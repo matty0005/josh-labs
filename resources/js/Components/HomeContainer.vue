ebfcf3af76d7a25bc546527da96420b2bdbbd352
jdw5 <joshuadwallace1@gmail.com>
2022-08-05 12:42:40 +1000

    Container component

---
+++ resources/js/Components/HomeContainer.vue
@@ -0,0 +1,28 @@
+<template>
+    <div class="relative group rounded flex items-end p-4 h-full bg-fixed" :class="`${bg} ${grid}`">
+            <div class="mt-auto bg-gray-50 rounded-sm px-4 py-2 w-fit tracking-wider lowercase shadow-inner font-light">
+                Example
+            </div>
+        </div>
+</template>
+
+<script>
+export default {
+    props: {
+        bg: {
+            type: String,
+            required: true,
+        },
+
+        grid: {
+            type: String,
+            required: true,
+        }
+    }
+
+}
+</script>
+
+<style>
+
+</style>
--- resources/js/Pages/Home.vue
+++ resources/js/Pages/Home.vue
@@ -1,31 +1,35 @@
 <template>
-    <div class="grid grid-cols-4 grid-flow-row gap-4 min-h-screen">
-        <div class="row-span-3 bg-gradient-to-b h-full from-indigo-500 to-blue-600 bg-fixed">01</div>
-
-
-        <div class="row-span-1 col-span-2 relative group rounded flex items-end p-4 bg-gradient-to-br h-full from-indigo-500 to-red-600 bg-fixed">
-            <div class="mt-auto bg-gray-50 rounded-sm px-4 py-2 w-fit tracking-wider lowercase shadow-inner font-light">
-                Menus
-            </div>
-        </div>
-
-
-        <div class="row-span-2 col-span-1 bg-black">03</div>
-        <div class="row-span-2 col-span-2 bg-black">04</div>
-        <div class="row-span-3 col-span-1 bg-black">05</div>
-        <div class="row-span-2 col-span-2 bg-black">06</div>
-        <div class="row-span-2 col-span-1 bg-black">07</div>
-
-
-
+    <div class="gap-4 min-h-screen" :class="!view ? 'flex flex-col' : 'grid grid-cols-4 grid-flow-row'">
+        <HomeContainer :grid="'row-span-3 col-span-1'" :bg="'bg-gradient-to-b h-full from-indigo-500 to-blue-600'"/>
+        <HomeContainer :grid="'row-span-1 col-span-2'" :bg="'bg-gradient-to-br from-black to-red-600 bg-fixed'"/>
+        <HomeContainer :grid="'row-span-2 col-span-1'" :bg="'bg-gradient-to-br from-black to-red-600 bg-fixed'"/>
+        <HomeContainer :grid="'row-span-2 col-span-2'" :bg="'bg-gradient-to-br from-black to-red-600 bg-fixed'"/>
+        <HomeContainer :grid="'row-span-3 col-span-1'" :bg="'bg-gradient-to-br from-black to-red-600 bg-fixed'"/>
+        <HomeContainer :grid="'row-span-2 col-span-2'" :bg="'bg-gradient-to-br from-black to-red-600 bg-fixed'"/>
+        <HomeContainer :grid="'row-span-2 col-span-1'" :bg="'bg-gradient-to-br from-black to-red-600 bg-fixed'"/>
     </div>
 </template>
 
 <script>
 import AppLayout from '@/Layouts/AppLayout.vue'
+import HomeContainer from '@/Components/HomeContainer.vue'
 
 export default {
+    components: {
+        HomeContainer
+    },
+
     layout: [AppLayout],
+
+    data() {
+        return {
+            view: 0
+        }
+    },
+
+    mounted() {
+        this.view = localStorage.getItem('view');
+    }
 
 }
 </script>
