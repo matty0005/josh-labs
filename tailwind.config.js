cb02f1a2ad207c1b0c3186072c547875396f2f08
jdw5 <joshuadwallace1@gmail.com>
2022-08-05 17:24:42 +1000

    Mesh gradient added to containers and tailwind animation

--- tailwind.config.js
+++ tailwind.config.js
@@ -14,6 +14,22 @@ module.exports = {
             fontFamily: {
                 sans: ['Nunito', ...defaultTheme.fontFamily.sans],
             },
+
+            backgroundSize: {
+                '4x' : '400%',
+            },
+
+            animation: {
+                gradient: 'gradient 5s ease infinite',
+            },
+
+            keyframes: {
+                gradient: {
+                    '0%' : { backgroundPosition: '0% 50%' },
+                    '50%' : { backgroundPosition: '100% 50%' },
+                    '100%' : { backgroundPosition: '0% 50%' },
+                },
+            },
         },
     },
 
